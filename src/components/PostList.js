0c64c337fd1e92e1d374ba3efcc205dfd0a576cf
Muhammad Talha waqar <talhawaqar007@gmail.com>
2021-06-17 05:04:58 +0500

    customize axios for postlist api

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends React.Component {
   render() {
+    this.props.fetchPosts();
     return(
       <div>Post list</div>
     );
   }
 }
 
-export default PostList;
+const mapStateToProps = (state) => {
+  console.log(state);
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  {fetchPosts}
+)(PostList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,10 @@
 import { combineReducers } from "redux";
 
 export default combineReducers({
-  replaceMe: () => 'hi',
+  posts: (posts = [], action) => {
+    if (action.type === 'FETCH_POSTS') {
+      return action.payload;
+    }
+    return posts;
+  },
 });
