c7e2d4b1b41d244e503d431d889e05acadfca283
Muhammad Talha waqar <talhawaqar007@gmail.com>
2021-06-19 22:59:12 +0500

    Display username

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,18 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async dispatch => {
-
   const response = await jsonPlaceholder.get('posts');
-
   dispatch({
     type: 'FETCH_POSTS',
     payload: response
+  });
+};
+
+export const fetchUser = (userId) => async dispatch => {
+  const response = await jsonPlaceholder.get(`users/${userId}`);
+
+  dispatch({
+    type: 'FETCH_USER',
+    payload: response.data
   });
 };
