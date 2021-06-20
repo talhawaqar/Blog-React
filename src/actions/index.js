a5fd57f24eccb500323344391f36eb475331d194
Muhammad Talha waqar <talhawaqar007@gmail.com>
2021-06-20 10:53:29 +0500

    Enhance api call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,18 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 import _ from 'lodash';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const posts = getState().posts;
+  // const userIds = _.uniq(_.map(posts, 'userId'));
+  // userIds.forEach( id => dispatch(fetchUser(id)));
+  _.chain(posts)
+    .map('userId')
+    .uniq()
+    .forEach( id => dispatch(fetchUser(id)))
+    .value();
+};
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('posts');
   dispatch({
@@ -9,12 +21,18 @@ export const fetchPosts = () => async dispatch => {
   });
 };
 
-export const fetchUser = (userId) => dispatch => _fetchUser(userId, dispatch);
-
-const _fetchUser = _.memoize(async (userId, dispatch) => {
+export const fetchUser = (userId) => async dispatch => {
   const response = await jsonPlaceholder.get(`users/${userId}`);
   dispatch({
     type: 'FETCH_USER',
     payload: response.data
   });
-});
+};
+
+// const _fetchUser = _.memoize(async (userId, dispatch) => {
+//   const response = await jsonPlaceholder.get(`users/${userId}`);
+//   dispatch({
+//     type: 'FETCH_USER',
+//     payload: response.data
+//   });
+// });
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
 import UserHeader from './UserHeader';
+import { fetchPostsAndUsers } from '../actions';
 
 class PostList extends React.Component {
   
   componentDidMount() {
-    this.props.fetchPosts();
+    this.props.fetchPostsAndUsers();
   }
 
   renderList() {
@@ -41,5 +41,5 @@ const mapStateToProps = (state) => {
 
 export default connect(
   mapStateToProps,
-  {fetchPosts}
+  {fetchPostsAndUsers}
 )(PostList);
